ca8f2c96c27dac7bd64a2f35cdcba37059d4c3b0
Murathotti Sivapriya <sivapriya.skht@gmail.com>
2026-04-27 14:09:30 +0530

    Update SecurityConfig.java

--- backend/backend/src/main/java/com/taskvertex/backend/security/SecurityConfig.java
+++ backend/backend/src/main/java/com/taskvertex/backend/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // Vite default port
+        config.setAllowedOrigins(List.of("http://localhost:3000" , "https://taskvertex-frontend.vercel.app")); 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
